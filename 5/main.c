1a531578b14773fea272b2f1a2ffab333e267cf7
koziolekrg <koziolekrg@gmail.com>
2016-10-31 09:33:56 +0000

    task 5 finished

--- 5/main.c
+++ 5/main.c
@@ -1,33 +1,55 @@
-#include "compress.h"
 #include <stdio.h>
 #include <string.h>
-   
+#include <stdlib.h>
 
-char* StrCompress(char *myStr)
-{
-  char *s, *in;
-  for (s = myStr, in = myStr; *s; s++) {
-    int count = 1;
-    in[0] = s[0]; in++;
-    while (s[0] == s[1]) {
-      count++;
-      s++;
+
+void compress(char *str){
+
+    char *astr = malloc(strlen(str)*sizeof(char));
+    char *temp = malloc(strlen(str)*sizeof(char));
+
+    memcpy(temp, str, strlen(str));
+
+
+
+    for(astr, temp; *temp; temp++){
+        int count = 1;
+        astr[0] = temp[0];
+        while(temp[0] == temp[1]){
+            count++;
+            temp++;
+        }
+
+        if(count > 1){
+            printf("%s",astr);
+            printf("%d", count);
+        }
+        else{
+            printf("%s",astr);
+
+        }
     }
-    if (count > 1) {
-      int len = sprintf(in, "%d", count);
-      in += len;
-    }
-  }
-  in[0] = 0;
-  return myStr;
+
 }
 
 
 int main(int argc, char* argv[]){
 
- char string[] = "pppqqaweeel";
 
- printf("%s",StrCompress(string));
+    char *str = malloc(sizeof(char) * 128);
+
+    printf("Enter text you want to compress: ");
+    scanf("%126s", str);
+
+
+    printf("origin: %s\n",str);
+
+    printf("compressed: ");
+    compress(str);
+
+    printf("\n");
+
+    free(str);
 
     
     return 0;
